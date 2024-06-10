d686c8f80ddb9c4699dd4c942a208fd71c14e946
Miller <miltonycchow@gmail.com>
2024-06-10 21:44:39 +0800

    feat: OOP

---
+++ utils/MathForOliver.py
@@ -0,0 +1,12 @@
+class MathForOliver:
+    @staticmethod
+    def add(a, b):
+        return print(a + b)
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
